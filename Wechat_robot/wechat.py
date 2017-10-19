28765849c3317905094efa249fd66720ab9ca820
Sundae Chen <sundaechn@gmail.com>
2017-10-19 19:43:11 +0800

    Update wechat.py

--- Wechat_robot/wechat.py
+++ Wechat_robot/wechat.py
@@ -1,31 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-10-19 19:42:44
+# @Author  : Sundae Chen (sundaechn@gmail.com)
+# @Link    : http://sundae.applinzi.com/home
+
+
 import itchat
 import re
-from urllib import request,parse
+from urllib import request, parse
 
-def Robot(content,toUser):
-    api='http://www.tuling123.com/openapi/api'
-    apikey='db33db678c254e33abc877db64548fb6'
-    data=parse.urlencode([('key',apikey),('info',content),('userid',toUser)])
-    req=request.Request(api)
-    response=request.urlopen(req,data.encode('utf-8'))
-    answer=response.read().decode('utf-8')
-    results=re.findall('"text":"(.*?)"',answer)
+
+def Robot(content, toUser):
+    api = 'http://www.tuling123.com/openapi/api'
+    apikey = 'db33db678c254e33abc877db64548fb6'
+    data = parse.urlencode(
+        [('key', apikey), ('info', content), ('userid', toUser)])
+    req = request.Request(api)
+    response = request.urlopen(req, data.encode('utf-8'))
+    answer = response.read().decode('utf-8')
+    results = re.findall('"text":"(.*?)"', answer)
     for result in results:
         return result
 
+
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
-	friend=itchat.search_friends(userName=msg['FromUserName'])
-	print(friend['NickName']+':'+msg['Text'])
-	text_send(msg)
+    friend = itchat.search_friends(userName=msg['FromUserName'])
+    print(friend['NickName']+':'+msg['Text'])
+    text_send(msg)
+
 
 def text_send(msg):
-	global words
-	text=Robot(msg['Text'],msg['FromUserName'])
-	text=text+'\n'+'('+words+')'
-	print('Sundae:'+text)
-	itchat.send(text, msg['FromUserName'])
+    global words
+    text = Robot(msg['Text'], msg['FromUserName'])
+    text = text+'\n'+'('+words+')'
+    print('Sundae:'+text)
+    itchat.send(text, msg['FromUserName'])
 
-words=input('请输入备注留言：')	
+words = input('请输入备注留言：')
 itchat.auto_login(hotReload=True)
 itchat.run(debug=True)
